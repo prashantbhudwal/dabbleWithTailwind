70eb73e8160ab5f208549af885fffc00134dae72
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-20 19:40:39 +0530

    Layout the Sidebar

--- Library/GridLayoutOne.tsx
+++ Library/GridLayoutOne.tsx
@@ -2,10 +2,10 @@ export default function GridLayoutOne() {
   return (
     <div className="mx-auto max-w-4xl flex-1 p-10">
       <ul className="grid grid-cols-12 grid-rows-6 gap-3">
-        <li className="rounded-lg bg-white shadow-xl">
+        <li className="col-span-3 col-start-1 row-span-4 row-start-2 rounded-lg bg-white shadow-xl ">
           <div className="h-24 p-3 text-center">Sidebar</div>
         </li>
-        <li className="col-span-full row-span-1 row-start-1 h-16 rounded-lg bg-white shadow-xl">
+        <li className="col-span-full row-span-1 row-start-1 rounded-lg bg-white shadow-xl">
           <div className="h-24 p-3 text-center"> Header</div>
         </li>
         <li className="col-span-full row-span-1 row-start-6 rounded-lg bg-white shadow-xl">
