64ab8a6e1842bba1d8c4028cfacd4aedef9e8d24
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-20 19:34:06 +0530

    Layout the Header and Footer

--- Library/GridLayoutOne.tsx
+++ Library/GridLayoutOne.tsx
@@ -1,41 +1,41 @@
 export default function GridLayoutOne() {
   return (
-    <div className="flex-1 max-w-4xl mx-auto p-10">
-      <ul className="grid grid-cols-3 gap-3">
-        <li className="bg-white rounded-lg shadow-xl">
+    <div className="mx-auto max-w-4xl flex-1 p-10">
+      <ul className="grid grid-cols-12 grid-rows-6 gap-3">
+        <li className="rounded-lg bg-white shadow-xl">
           <div className="h-24 p-3 text-center">Sidebar</div>
         </li>
-        <li className="bg-white rounded-lg shadow-xl">
+        <li className="col-span-full row-span-1 row-start-1 h-16 rounded-lg bg-white shadow-xl">
           <div className="h-24 p-3 text-center"> Header</div>
         </li>
-        <li className="bg-white rounded-lg shadow-xl">
+        <li className="col-span-full row-span-1 row-start-6 rounded-lg bg-white shadow-xl">
           <div className="h-24 p-3 text-center">Footer</div>
         </li>
-        <li className="bg-white rounded-lg shadow-xl">
+        <li className="rounded-lg bg-white shadow-xl">
           <div className="h-24 p-3 text-center">Hero Section</div>
         </li>
-        <li className="bg-white rounded-lg shadow-xl">
+        <li className="rounded-lg bg-white shadow-xl">
           <div className="h-24 p-3 text-center">Hero Image</div>
         </li>
-        <li className="bg-white rounded-lg shadow-xl">
+        <li className="rounded-lg bg-white shadow-xl">
           <div className="h-24 p-3 text-center">Hero Form</div>
         </li>
-        <li className="bg-white rounded-lg shadow-xl">
+        <li className="rounded-lg bg-white shadow-xl">
           <div className="h-24 p-3 text-center">Hero Content</div>
         </li>
-        <li className="bg-white rounded-lg shadow-xl">
+        <li className="rounded-lg bg-white shadow-xl">
           <div className="h-24 p-3 text-center">Features Section</div>
         </li>
-        <li className="bg-white rounded-lg shadow-xl">
+        <li className="rounded-lg bg-white shadow-xl">
           <div className="h-24 p-3 text-center">How it works</div>
         </li>
-        <li className="bg-white rounded-lg shadow-xl">
+        <li className="rounded-lg bg-white shadow-xl">
           <div className="h-24 p-3 text-center">Chat with us</div>
         </li>
-        <li className="bg-white rounded-lg shadow-xl">
+        <li className="rounded-lg bg-white shadow-xl">
           <div className="h-24 p-3 text-center">Another Section</div>
         </li>
-        <li className="bg-white rounded-lg shadow-xl">
+        <li className="rounded-lg bg-white shadow-xl">
           <div className="h-24 p-3 text-center">Another Section</div>
         </li>
       </ul>
